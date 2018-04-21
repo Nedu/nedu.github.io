9c0e9f93d859b5112234c78af411c90d41d2b1e8
Nedu Robert <nedumrobert@gmail.com>
2018-04-21 21:35:29 +0100

    implemented the navbar for the site

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,46 @@
-import React from 'react'
+import React, { Component} from 'react'
 import Link from 'gatsby-link'
+import './header.css'
+import FaBar from 'react-icons/lib/fa/bars'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { isToggleOn: true }
+  }
+
+  classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
+
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'))
+  }
+
+  render() {
+    return (
+      <div className="Navbar">
+        <div className="Navbar__Link Navbar__Link-brand">
+          <a href="/" className="Navbar__Link-brandLink" title="Nedu Robert">
+            <span>&lt;NR&gt;</span>
+          </a>
+        </div>
+        <div
+          className="Navbar__Link Navbar__Link-toggle"
+          onClick={this.classToggle}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+          {/* <i className="fas fa-bars" /> */}
+          <FaBar />
+        </div>
+        <nav className="Navbar__Items Navbar__Items--left">
+          <div className="Navbar__Link">Portfolio</div>
+          <div className="Navbar__Link">Resume</div>
+        </nav>
+        <nav className="Navbar__Items Navbar__Items--right">
+          <div className="Navbar__Link">Contact</div>
+          <div className="Navbar__Link">Blog</div>
+        </nav>
+      </div>
+    )
+  }
+}
 
 export default Header
