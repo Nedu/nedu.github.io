0ba82e7b7f95a9c375c338a0fd3a23b86e3386ae
Nedu Robert <nedumrobert@gmail.com>
2018-04-22 01:15:53 +0100

    changed section element to a div

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,13 @@ import Link from 'gatsby-link'
 import '../styles/index.css'
 
 const IndexPage = () => (
-    <section className="home">
+    <div className="home">
       <div className="intro__text">
         <h1>Nedu Robert</h1>
         <hr/>
         <h2>Full Stack Developer</h2>
       </div>
-		</section>
+		</div>
 )
 
 export default IndexPage
