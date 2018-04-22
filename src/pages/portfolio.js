611bf6b946573e304275bdd8a78c20608b347e10
Nedu Robert <nedumrobert@gmail.com>
2018-04-22 20:30:50 +0100

    implemented the portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -10,32 +10,112 @@ import wikiViewer from '../images/wikiviewer.png'
 import RQG from '../images/RQG.png'
 
 const PortfolioPage = () => (
-  <div className="portfolio">
-      <img
-        src={tribute}
-        alt="tribute webpage screenshot"
-      />
-      <img
-        src={imgGallery}
-        alt="image gallery screenshot"
-      />
-      <img
-        src={landingPage}
-        alt="landing page screenshot"
-      />
-      <img
-        src={color}
-        alt="color game app screenshot"
-      />
-      <img
-        src={wikiViewer}
-        alt="wiki viewer web app screenshot"
-      />
-      <img
-        src={RQG}
-        alt="random quote generator project webpage screenshot"
-      />
-  </div>
+    <div className="portfolio">
+        <div className="portfolio__project">
+            <div className="portfolio__project-demo">
+                <img 
+                    src={tribute} 
+                    alt="tribute webpage screenshot"
+                />
+            </div>
+            <div className="portfolio__project-description">
+                <h3>Responsive Website</h3>
+                <p>Simple single page reponsive tribute website to Superman</p>
+                <p><span className="skilltag">HTML5</span><span className="skilltag">CSS3</span><span className="skilltag">Bootstrap 4</span><span className="skilltag">Responsive Design</span></p>
+                <div className="modallinks">
+                <Link className="btn btn-primary" to ="https://github.com/Nedu/Tribute-Page" target="_blank">GitHub</Link>
+                <Link className="btn btn-primary" to="https://nedu.github.io/Tribute-Page/" target="_blank">View Live</Link>
+                </div>
+            </div>
+        </div>
+        <div className="portfolio__project">
+            <div className="portfolio__project-demo">            
+                <img
+                    src={imgGallery}
+                    alt="image gallery screenshot"
+                />
+            </div>
+            <div className="portfolio__project-description">
+                <h3>Image Gallery</h3>
+                <p>Simple image gallery</p>
+                <p><span className="skilltag">HTML5</span><span className="skilltag">CSS3</span><span className="skilltag">Responsive Design</span></p>
+                <div className="modallinks">
+                <a className="btn btn-primary" href ="https://github.com/Nedu/ImageGallery" target="_blank">GitHub</a>
+                <a className="btn btn-primary" href="https://nedu.github.io/ImageGallery/" target="_blank">View Live</a>
+                </div>
+            </div>
+        </div>
+        <div className="portfolio__project">
+            <div className="portfolio__project-demo">
+                <img
+                    src={landingPage}
+                    alt="landing page screenshot"
+                />
+            </div>
+            <div className="portfolio__project-description">
+                <h3>Landing Page</h3>
+                <p>Simple landing page</p>
+                <p><span className="skilltag">HTML5</span><span className="skilltag">CSS3</span><span className="skilltag">Responsive Design</span><span className="skilltag">Bootstrap 3</span></p>
+                <div className="modallinks">
+                <a className="btn btn-primary" href ="https://github.com/Nedu/LandingPage" target="_blank">GitHub</a>
+                <a className="btn btn-primary" href="https://nedu.github.io/LandingPage/" target="_blank">View Live</a>
+                </div>
+            </div>
+        </div>
+        <div className="portfolio__project">
+            <div className="portfolio__project-demo">
+                <img
+                    src={color}
+                    alt="color game app screenshot"
+                />
+            </div>
+            <div className="portfolio__project-description">
+                <h3>Color Game</h3>
+                <p>Simple Color Game where you guess a color based on a given RGB value</p>
+                <p><span className="skilltag">HTML5</span><span className="skilltag">CSS3</span><span className="skilltag">Responsive Design</span><span className="skilltag">JavaScript</span></p>
+                <div className="modallinks">
+                <a className="btn btn-primary" href ="https://github.com/Nedu/ColorGame" target="_blank">GitHub</a>
+                <a className="btn btn-primary" href="https://nedu.github.io/ColorGame/" target="_blank">View Live</a>
+                </div>
+            </div>
+        </div>
+        <div className="portfolio__project">
+            <div className="portfolio__project-demo">
+                <img
+                    src={wikiViewer}
+                    alt="wiki viewer web app screenshot"
+                />
+            </div>
+            <div className="portfolio__project-description">
+                <h3>WikiSearch</h3>
+                <p>A Wikipedia article previewer built using React that utilizes the MediaWiki API to search for the user's query.</p>
+                <p><span className="skilltag">React.js</span><span className="skilltag">Webpack</span><span className="skilltag">JavaScript</span><span className="skilltag">Wikipedia API</span></p>
+                <div className="modallinks">
+                <a className="btn btn-primary" href ="https://github.com/Nedu/WikiSearch" target="_blank">GitHub</a>
+                <a className="btn btn-primary" href="https://nedu.github.io/WikiSearch/" target="_blank">View Live</a>
+                </div>
+            </div>
+        </div>
+        <div className="portfolio__project">
+            <div className="portfolio__project-demo">
+                <img
+                    src={RQG}
+                    alt="random quote generator project webpage screenshot"
+                />
+            </div>
+            <div className="portfolio__project-description">
+                <h3>Random Quote Generator</h3>
+                <p>A Random Quote Generator built using AJAX utilizing a JSON-encoded, third-party external API to fetch quote data. Also integrated Twitter Web Intents to enable users tweet out quotes.</p>
+                <p><span className="skilltag">JavaScript (ES6)</span><span className="skilltag">AJAX</span><span className="skilltag">Fetch API</span><span className="skilltag">Quotes API</span></p>
+                <div className="modallinks">
+                <a className="btn btn-primary" href ="https://github.com/Nedu/RQG" target="_blank">GitHub</a>
+                <a className="btn btn-primary" href="https://nedu.github.io/RQG/" target="_blank">View Live</a>
+                </div>
+            </div>
+        </div> 
+    </div> 
 )
+    
+
 
 export default PortfolioPage
