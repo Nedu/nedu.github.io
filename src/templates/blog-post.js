ffe93087c10b51f29909c48c581537819ddd5522
Nedu Robert <nedumrobert@gmail.com>
2018-11-14 15:30:47 -0700

    Added styling for katex

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import '../styles/blog.css'
+import "katex/dist/katex.min.css"
 
 export default function Template({data}) {
     const post = data.markdownRemark 
