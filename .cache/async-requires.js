3629d0d2859fa64996ae209ab60ac118f860ee26
Nedu Robert <nedumrobert@gmail.com>
2018-11-14 15:59:05 -0700

    Added files to gitignore

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,7 +3,6 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-templates-blog-post-js": require("gatsby-module-loader?name=component---src-templates-blog-post-js!/mnt/c/Users/Chinedum/Desktop/Lambda/Portfolio/nedu.github.io/src/templates/blog-post.js"),
-  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/mnt/c/Users/Chinedum/Desktop/Lambda/Portfolio/nedu.github.io/.cache/dev-404-page.js"),
   "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/mnt/c/Users/Chinedum/Desktop/Lambda/Portfolio/nedu.github.io/src/pages/404.js"),
   "component---src-pages-blog-js": require("gatsby-module-loader?name=component---src-pages-blog-js!/mnt/c/Users/Chinedum/Desktop/Lambda/Portfolio/nedu.github.io/src/pages/blog.js"),
   "component---src-pages-contact-js": require("gatsby-module-loader?name=component---src-pages-contact-js!/mnt/c/Users/Chinedum/Desktop/Lambda/Portfolio/nedu.github.io/src/pages/contact.js"),
@@ -15,9 +14,7 @@ exports.components = {
 
 exports.json = {
   "layout-index.json": require("gatsby-module-loader?name=path---!/mnt/c/Users/Chinedum/Desktop/Lambda/Portfolio/nedu.github.io/.cache/json/layout-index.json"),
-  "100-web-projects.json": require("gatsby-module-loader?name=path---100-web-projects!/mnt/c/Users/Chinedum/Desktop/Lambda/Portfolio/nedu.github.io/.cache/json/100-web-projects.json"),
   "converting-from-a-truth-table-to-a-boolean-expression.json": require("gatsby-module-loader?name=path---converting-from-a-truth-table-to-a-boolean-expression!/mnt/c/Users/Chinedum/Desktop/Lambda/Portfolio/nedu.github.io/.cache/json/converting-from-a-truth-table-to-a-boolean-expression.json"),
-  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/mnt/c/Users/Chinedum/Desktop/Lambda/Portfolio/nedu.github.io/.cache/json/dev-404-page.json"),
   "404.json": require("gatsby-module-loader?name=path---404!/mnt/c/Users/Chinedum/Desktop/Lambda/Portfolio/nedu.github.io/.cache/json/404.json"),
   "blog.json": require("gatsby-module-loader?name=path---blog!/mnt/c/Users/Chinedum/Desktop/Lambda/Portfolio/nedu.github.io/.cache/json/blog.json"),
   "contact.json": require("gatsby-module-loader?name=path---contact!/mnt/c/Users/Chinedum/Desktop/Lambda/Portfolio/nedu.github.io/.cache/json/contact.json"),
