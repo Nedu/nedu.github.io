6c6cb6ee5c0a6d5a0d3d1bddfd78b08bf3b1cf94
Nedu Robert <nedumrobert@gmail.com>
2018-11-14 15:29:08 -0700

    Implemented gatsby-remark-katex as a plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,13 @@ module.exports = {
         name: 'pages'
       }
     },
-    'gatsby-transformer-remark'
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-katex'
+        ]
+      }
+    } 
   ],
 }
