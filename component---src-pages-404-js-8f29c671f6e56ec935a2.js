e3897bf7e18af0242a765e31794aa9d975044a81
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-15 21:21:20 +0000

    Deploy Nedu/nedu.github.io to github.com/Nedu/nedu.github.io.git:master

---
+++ component---src-pages-404-js-8f29c671f6e56ec935a2.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x9427c64ab85d],{201:function(e,t,u){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0;var n=u(1),l=a(n),s=function(){return l.default.createElement("div",{className:"notFoundPage"},l.default.createElement("h1",null,"NOT FOUND"),l.default.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))};t.default=s,e.exports=t.default}});
+//# sourceMappingURL=component---src-pages-404-js-8f29c671f6e56ec935a2.js.map
